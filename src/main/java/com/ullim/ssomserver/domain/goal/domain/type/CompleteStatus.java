e87e9ce55b2b19c94d59dcb7e562b8225ae8a6f6
YuLim2 <jyulim0120@gmail.com>
2023-03-09 19:50:58 +0900

    ADD: Goal delete and complete status enum

---
+++ src/main/java/com/ullim/ssomserver/domain/goal/domain/type/CompleteStatus.java
@@ -0,0 +1,5 @@
+package com.ullim.ssomserver.domain.goal.domain.type;
+
+public enum CompleteStatus {
+    NOT_COMPLETED, COMPLETED
+}
---
+++ src/main/java/com/ullim/ssomserver/domain/goal/domain/type/GoalStatus.java
@@ -0,0 +1,5 @@
+package com.ullim.ssomserver.domain.goal.domain.type;
+
+public enum GoalStatus {
+    NOT_DELETED, DELETED
+}
