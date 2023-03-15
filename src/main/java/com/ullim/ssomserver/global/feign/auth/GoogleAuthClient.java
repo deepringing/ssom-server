4cd7c7dfa4cd394217bb49440211342aaf1f7c39
YuLim2 <jyulim0120@gmail.com>
2023-03-15 17:02:06 +0900

    ADD: Google auth client

---
+++ src/main/java/com/ullim/ssomserver/global/feign/auth/GoogleAuthClient.java
@@ -0,0 +1,14 @@
+package com.ullim.ssomserver.global.feign.auth;
+
+import com.ullim.ssomserver.global.feign.auth.dto.request.GoogleAuthRequest;
+import com.ullim.ssomserver.global.feign.auth.dto.response.GoogleTokenResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = "GoogleAuthClient", url = "https://oauth2.googleapos.com/token")
+public interface GoogleAuthClient {
+
+    @PostMapping
+    public GoogleTokenResponse getAccessToken(GoogleAuthRequest request);
+
+}
